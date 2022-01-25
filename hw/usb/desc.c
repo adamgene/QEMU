0414cfa65c80b92e47ae1e5eec0b0e2cf5c1dee9
Douglas Chen <dougpuob@gmail.com>
2022-01-25 16:53:13 +0800

    Refine source code (Suggestions from reviewer Adam)
    Suggestions:
    - Removed the `ven_code` parameter from usb_desc_msos_str_desc() function (get it from `desc->msos->bMS_VendorCode`).
    - Applied usb_lo() and usb_hi() macro functions.
    - Changed vendor code of WinUsb to `0xAA` (bMS_VendorCode).
    - Changed `idProduct` and `bcdDevice` to non-meaning values.
    
    Others:
    - Add three variables `bMS_VendorCode`, `RegistryPropertyName` and `RegistryPropertyData` to struct USBDescMSOS.
    - Remove `desc->msos->Label` because it seems an unnecessary example (replaced with RegistryPropertyXxxx).
    - Support bmRequestType for `0xC1`.

--- hw/usb/desc.c
+++ hw/usb/desc.c
@@ -2,6 +2,7 @@
 
 #include "hw/usb.h"
 #include "desc.h"
+#include "desc-msos.h"
 #include "trace.h"
 
 /* ------------------------------------------------------------------ */
@@ -511,7 +512,7 @@ void usb_desc_init(USBDevice *dev)
     }
     if (desc->msos && (dev->flags & (1 << USB_DEV_FLAG_MSOS_DESC_ENABLE))) {
         dev->flags |= (1 << USB_DEV_FLAG_MSOS_DESC_IN_USE);
-        usb_desc_set_string(dev, 0xee, "MSFT100Q");
+        usb_desc_set_string(dev, MSOS_DESC_INDEX, "MSFT100");
     }
     usb_desc_setdefaults(dev);
 }
@@ -610,6 +611,12 @@ int usb_desc_string(USBDevice *dev, int index, uint8_t *dest, size_t len)
     }
 
     str = usb_desc_get_string(dev, index);
+	if (index == MSOS_DESC_INDEX) {
+        if (dev->flags & (1 << USB_DEV_FLAG_MSOS_DESC_IN_USE)) {
+            return usb_desc_msos_str_desc(desc, str, dest);
+        }
+    }
+
     if (str == NULL) {
         str = usb_device_get_usb_desc(dev)->str[index];
         if (str == NULL) {
@@ -625,15 +632,6 @@ int usb_desc_string(USBDevice *dev, int index, uint8_t *dest, size_t len)
         dest[pos++] = str[i++];
         dest[pos++] = 0;
     }
-
-    if (desc->msos && desc->msos->IsWinUsb) {
-        // 'MSFT100Q' --> 'MSFT100\0'
-        if (0 == strcmp("MSFT100Q", str)) {
-            dest[bLength - 1] = 0x00;
-            dest[bLength - 2] = 0x00;
-        }
-    }
-
     return pos;
 }
 
